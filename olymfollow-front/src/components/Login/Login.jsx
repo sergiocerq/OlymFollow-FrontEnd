31dace6aba2e47eb60495a96c5c9da88e6679366
vlopes <victorlopes9291@gmail.com>
2024-08-12 13:28:14 -0300

    feat: add login form

--- olymfollow-front/src/components/Login/Login.jsx
+++ olymfollow-front/src/components/Login/Login.jsx
@@ -4,7 +4,7 @@ import { FetcherFactory } from "../../data/fetchers/FetcherFactory";
 
 const fetcherFactory = new FetcherFactory();
 
-export const Login = () => {
+const Login = () => {
 
   const [login, setLogin] = useState({
     email: "",
@@ -16,5 +16,43 @@ export const Login = () => {
     const user = await loginFetcher.login(login.email, login.password);
   }
 
-  return <div>Login</div>;
-};
+  return (
+      <>
+        <form className="form">
+          <p className="form-title">Entre na sua conta</p>
+          <div className="input-container">
+            <input placeholder="Digite o email" type="email"/>
+            <span>
+            <svg stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  strokeWidth="2" strokeLinejoin="round" strokeLinecap="round"></path>
+            </svg>
+          </span>
+          </div>
+          <div className="input-container">
+            <input placeholder="Digite a senha" type="password"/>
+
+            <span>
+            <svg stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" strokeWidth="2" strokeLinejoin="round"
+                    strokeLinecap="round"></path>
+              <path
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  strokeWidth="2" strokeLinejoin="round" strokeLinecap="round"></path>
+            </svg>
+          </span>
+          </div>
+          <button className="submit" type="submit">
+            Sign in
+          </button>
+
+          <p className="signup-link">
+            Sem conta?
+            <a href="">Cadastrar</a>
+          </p>
+        </form>
+      </>
+  );
+}
+export default Login;
