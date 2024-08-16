d8cadc217ab3bf98a36ea471b895778699cb9dd2
victor <victorlopes9291@gmail.com>
2024-08-15 21:16:44 -0300

    feat: add a requisição para pegar usuário no componente settings

--- olymfollow-front/src/components/Settings/Settings.jsx
+++ olymfollow-front/src/components/Settings/Settings.jsx
@@ -1,33 +1,44 @@
-import Lottie from "lottie-react";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
-import animationData from "../../assets/LoadingAnimation.json";
-import { QuadroMedalhas } from "../Medalhas/QuadroMedalhas";
-import OlympicRings from "../../assets/Olympic_rings.svg";
 import "./Settings.css";
 import {NavBar} from "../navbar/NavBar.jsx";
 import {PaisesFavoritados} from "../Paises/PaisesFavoritados.jsx";
+import { FetcherFactory } from '../../data/fetchers/FetcherFactory';
+
+const fecherFactory = new FetcherFactory();
 
 export const Settings = () => {
   const navigate = useNavigate();
 
   const hasToken = sessionStorage.getItem("token");
+  const [favoritesCountries, setFavoritesCountries] = useState([]);
+
+  useEffect(()=>{
+      const fetchUser = async () => {
+          const userFetcher = fecherFactory.createUserFetcher();
+          const user = await userFetcher.getCurrentUser();
+          console.log(user.inscricoes)
+          setFavoritesCountries(user.inscricoes)
+      }
+      fetchUser();
+  },[])
 
   return (
     <>
       {hasToken && <NavBar />}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: " center",
-          gap: "2rem",
-          marginTop: "7rem",
-        }}
-      >
-      <PaisesFavoritados />
-        <Divider />
-      </div>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: " center",
+                gap: "2rem",
+                marginTop: "7rem",
+            }}
+        >
+            <h1>Países favoritos</h1>
+            <PaisesFavoritados countries={favoritesCountries}/>
+            <Divider/>
+        </div>
     </>
   );
 };
