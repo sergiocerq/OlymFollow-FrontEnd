ae77990d651ad96a55a78810e0ccad9c00261c06
victor <victorlopes9291@gmail.com>
2024-08-17 09:31:31 -0300

    fix: removendo import's

--- olymfollow-front/src/components/Register/Register.jsx
+++ olymfollow-front/src/components/Register/Register.jsx
@@ -1,10 +1,8 @@
 import { useRegister } from "./RegisterHooks";
 import "./Register.css";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import { Toaster } from "sonner";
 import googleSVG from "../../assets/google-color-svgrepo-com.svg";
-import sportsLogin from "../../assets/LottieLanding.json";
 import animationLogin from "../../assets/AnimationLogin.json";
 import Lottie from "lottie-react";
 
@@ -68,11 +66,11 @@ const Register = () => {
                 viewBox="0 0 32 32"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g
                   id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></g>
                 <g id="SVGRepo_iconCarrier">
                   <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"></path>
